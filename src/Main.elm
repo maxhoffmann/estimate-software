9d1ba008f7c0aa401d0641f83ceb951d8c5628ad
Maximilian Hoffmann <hey@max.hn>
2020-09-18 21:37:12 +0200

    add button

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (text)
+import Html exposing (Html, text)
+import Html.Attributes exposing (class)
 
 
 
@@ -9,13 +10,25 @@ import Html exposing (text)
 
 
 type alias Model =
-    { text : String
+    { tasks : Tasks
     }
+
+
+type alias Days =
+    Int
+
+
+type Task
+    = Task String Days Tasks
+
+
+type alias Tasks =
+    List Task
 
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    ( { text = "Hello World" }, Cmd.none )
+    ( { tasks = [ Task "test" 3 [ Task "subtask" 2 [] ] ] }, Cmd.none )
 
 
 
@@ -24,12 +37,16 @@ init _ =
 
 type Msg
     = NoOp
+    | AddTask
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
+            ( model, Cmd.none )
+
+        AddTask ->
             ( model, Cmd.none )
 
 
@@ -41,8 +58,23 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Estimates"
     , body =
-        [ text model.text ]
+        [ Html.main_ [ class "app" ]
+            [ Html.ul [ class "list" ] <|
+                (model.tasks
+                    |> List.map taskView
+                )
+            ]
+        ]
     }
+
+
+taskView : Task -> Html Msg
+taskView (Task description days tasks) =
+    Html.li [ class "task" ]
+        [ Html.span [] [ text (description ++ " " ++ String.fromInt days) ]
+        , Html.ul [ class "subtasks" ] <| List.map taskView tasks
+        , Html.button [ class "add" ] [ text "add" ]
+        ]
 
 
 
