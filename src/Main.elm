130ed15ffba93af2faa0a9a3aac81cd70b61ca28
Maximilian Hoffmann <hey@max.hn>
2020-09-18 22:47:55 +0200

    add new tasks

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, text)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import List.Extra exposing (getAt, setAt)
 
 
 
@@ -28,7 +30,16 @@ type alias Tasks =
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    ( { tasks = [ Task "test" 3 [ Task "subtask" 2 [] ] ] }, Cmd.none )
+    ( { tasks =
+            [ Task "test"
+                3
+                [ Task "subtask" 2 []
+                , Task "another" 1 []
+                ]
+            ]
+      }
+    , Cmd.none
+    )
 
 
 
@@ -37,17 +48,46 @@ init _ =
 
 type Msg
     = NoOp
-    | AddTask
+    | AddTask (List Int)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
+    case Debug.log "msg" msg of
         NoOp ->
             ( model, Cmd.none )
 
-        AddTask ->
-            ( model, Cmd.none )
+        AddTask path ->
+            ( { model | tasks = addTask path model.tasks }, Cmd.none )
+
+
+addTask : List Int -> Tasks -> Tasks
+addTask path tasks =
+    case Debug.log "path" path of
+        index :: restOfPath ->
+            let
+                taskAtIndex =
+                    getAt index tasks
+
+                _ =
+                    Debug.log "task" taskAtIndex
+
+                _ =
+                    Debug.log "index" index
+            in
+            if List.length restOfPath == 0 then
+                tasks ++ [ Task "new task" 0 [] ]
+
+            else
+                case taskAtIndex of
+                    Just (Task desc estimate subtasks) ->
+                        setAt index (Task desc estimate (addTask restOfPath subtasks)) tasks
+
+                    Nothing ->
+                        tasks
+
+        [] ->
+            tasks
 
 
 
@@ -61,19 +101,23 @@ view model =
         [ Html.main_ [ class "app" ]
             [ Html.ul [ class "list" ] <|
                 (model.tasks
-                    |> List.map taskView
+                    |> List.indexedMap (taskView [] model.tasks)
                 )
             ]
         ]
     }
 
 
-taskView : Task -> Html Msg
-taskView (Task description days tasks) =
+taskView : List Int -> Tasks -> Int -> Task -> Html Msg
+taskView path parentTasks index (Task description days tasks) =
     Html.li [ class "task" ]
         [ Html.span [] [ text (description ++ " " ++ String.fromInt days) ]
-        , Html.ul [ class "subtasks" ] <| List.map taskView tasks
-        , Html.button [ class "add" ] [ text "add" ]
+        , Html.ul [ class "subtasks" ] <| List.indexedMap (taskView (path ++ [ index ]) tasks) tasks
+        , if List.length parentTasks == index + 1 then
+            Html.button [ class "add", onClick (AddTask (path ++ [ index ])) ] [ text "add" ]
+
+          else
+            text ""
         ]
 
 
